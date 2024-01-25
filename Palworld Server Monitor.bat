8a7f5326aece0ab1b5e13fd6cbb5b4e7c5a8bf5a
Andrew Rutherford <60053428+Andrew1175@users.noreply.github.com>
2024-01-25 14:17:42 -0500

    Create Palworld Server Monitor.bat

---
+++ Palworld Server Monitor.bat
@@ -0,0 +1,16 @@
+@echo off
+set task1=PalServer-Win64-Test-Cmd
+
+tasklist | find /i "%task1%" > nul
+if %errorlevel% neq 0 (
+    echo %task1% is not running. Starting...
+	call "F:\palworld\Start Server.bat"
+	echo Sending email notification
+	Powershell.exe -File "C:\Input\Directory\Here\Palworld Send Email.ps1"
+	echo Email Sent
+) else (
+    echo %task1% is already running.
+)
+echo exiting in 5 seconds...
+timeout 5
+exit
